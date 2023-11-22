b305ce4d7af140875653f56f2ca61645202f9f63
Konstantinos <konstantinos.kitsios@uzh.ch>
2023-11-22 16:17:27 +0200

    Initial commit

---
+++ bcb_1vsAll_utilities.py
@@ -0,0 +1,72 @@
+import psycopg2
+import pandas as pd
+import os
+
+cache_dir = '/Users/konstantinos/Desktop/Clone Generalization/binary files/'
+
+# Database connection parameters
+db_params = {
+    "host"    : "localhost",
+    "database": "bcb",
+    "user"    : "konstantinos",
+    "password": "1234"
+}
+
+# Establish connection
+conn = psycopg2.connect(**db_params)
+
+# # Get all functionalities
+# functionalities = pd.read_sql_query("SELECT * from functionalities", conn)
+
+# Fetches at most N rows for each functionality ID. If a functionality ID has only M<N rows, it fetches them all.
+# The rows could be either clones (fetch_clones=True) or non-clones (fetch_clones=False). Resulting table has columns
+# code1 | code2 | label (0/1) | functionality_id
+def fetch_functionality_data(fetch_clones=True, max_rows_per_functionality=0):
+    if fetch_clones:
+        tableName   = "clones"
+        label_value = 1
+    else:
+        tableName   = "false_positives"
+        label_value = 0
+
+    # Partition by functionality ID to obtain a row number that resets when functionality changes. Then,
+    # fetch all rows with row_num<N
+    query = """
+    WITH RandomizedRows AS (
+      SELECT
+        function_id_one, function_id_two, functionality_id, %d as label,
+        ROW_NUMBER() OVER (PARTITION BY functionality_id ORDER BY random()) AS row_num
+      FROM %s
+      WHERE syntactic_type=3    
+    )
+    SELECT
+      f1.text AS code1, f2.text AS code2, R.label, R.functionality_id
+    FROM
+      RandomizedRows AS R
+      INNER JOIN
+      pretty_printed_functions AS f1 ON R.function_id_one=f1.function_id
+      INNER JOIN 
+      pretty_printed_functions AS f2 ON R.function_id_two=f2.function_id
+    WHERE R.row_num <= %d;
+    """ % (label_value, tableName, max_rows_per_functionality)
+    
+    data = pd.read_sql_query(query, conn)
+    return data
+
+def get_functionality_data(max_rows_per_functionality):
+    data_pairs_all_fname       = cache_dir + 'bcb_1vsAll/data_pairs_all_' + str(max_rows_per_functionality) + '.pickle'
+
+    if os.path.isfile(data_pairs_all_fname):
+        data_pairs_all = pd.read_pickle(data_pairs_all_fname)
+        print("Loaded data_pairs_all_%d.pickle from cache..." % (max_rows_per_functionality))
+    else:
+        data_true      = fetch_functionality_data(fetch_clones=True , max_rows_per_functionality=max_rows_per_functionality)
+        data_false     = fetch_functionality_data(fetch_clones=False, max_rows_per_functionality=max_rows_per_functionality)
+        # Avoid creating unbalanced dataset
+        n_rows_to_keep = min(len(data_true), len(data_false))
+        data_true      = data_true.sample(n_rows_to_keep)
+        data_false     = data_false.sample(n_rows_to_keep)
+        data_pairs_all = pd.concat([data_true, data_false]).reset_index()
+        data_pairs_all.to_pickle(data_pairs_all_fname)
+
+    return data_pairs_all
---
+++ scb_utilities.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import os
+
+
+def remove_outer_class_and_comments(java_code):
+    start_index = java_code.find('public class Clone')
+    end_index = java_code.find('*/', start_index)
+
+    if start_index != -1 and end_index != -1:
+        result_code = java_code[:start_index] + java_code[end_index + 2:]
+    else:
+        result_code = java_code
+        
+    result_code = result_code.strip()
+    
+    # Remove the "}" at the end of the file
+    result_code = result_code.rstrip('}')
+
+    return result_code
+
+def combine_to_create_false_pairs(data_scb):
+    false_pairs = []
+    # Iterate over the first half of the original DataFrame
+    for i in range(len(data_scb) // 2):
+        # Combine "code1" of the current row with "code1" of the corresponding row in the second half
+        new_code1 = data_scb.at[i, 'code1']
+        # Get "code2" of the current row
+        new_code2 = data_scb.at[i + len(data_scb) // 2, 'code1']
+
+        # Combine "code1" of the current row with "code1" of the corresponding row in the second half
+        new_code3 = data_scb.at[i, 'code2']
+        # Get "code2" of the current row
+        new_code4 = data_scb.at[i + len(data_scb) // 2, 'code2']
+        
+        # Set label to 0
+        label = 0
+        
+        # Append a new row to the new DataFrame
+        false_pairs.append({'code1': new_code1, 'code2': new_code2, 'label': label})
+        false_pairs.append({'code1': new_code3, 'code2': new_code4, 'label': label})
+
+    data_scb_false_pairs = pd.DataFrame(false_pairs)
+
+    data_scb = pd.concat([data_scb, data_scb_false_pairs]).reset_index()
+
+    return data_scb
+
+
+def get_data_scb():
+    # Path to the folder containing .java files
+    folder_path = "../Semantic Benchmark/Java/Stand Alone Clones"
+
+    # List to store data
+    java_data = []
+
+    # Iterate through each file in the folder
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".java"):
+            file_path = os.path.join(folder_path, filename)
+            
+            # Read the content of the Java file
+            with open(file_path, 'r') as file:
+                java_code = file.read()
+
+            java_code = remove_outer_class_and_comments(java_code)
+            
+            t = java_code.split('\n\n')
+            if len(t) != 3:
+                print(t)
+                continue
+
+            java_data.append({'filename':filename, 'code1':t[0], 'code2':t[1], 'label':1})
+            
+
+    # Create a DataFrame
+    data_scb = pd.DataFrame(java_data)
+
+    data_scb = combine_to_create_false_pairs(data_scb)
+
+    return data_scb
