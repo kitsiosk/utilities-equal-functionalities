e6d7617f53ea35365d45091033153e22b5582e6b
Konstantinos <konstantinos.kitsios@uzh.ch>
2023-11-22 22:36:13 +0100

    Better balanced dataset

--- bcb_1vsAll_utilities.py
+++ bcb_1vsAll_utilities.py
@@ -1,17 +1,17 @@
-import psycopg2
 import pandas as pd
 import os
 
 cache_dir = '/Users/konstantinos/Desktop/Clone Generalization/binary files/' # my Mac
 if not os.path.isdir(cache_dir):
-    cache_dir = '/home/kkitsi/data' # cluster
+    cache_dir = '/home/kkitsi/data/' # cluster
     
 
 # Fetches at most N rows for each functionality ID. If a functionality ID has only M<N rows, it fetches them all.
 # The rows could be either clones (fetch_clones=True) or non-clones (fetch_clones=False). Resulting table has columns
 # code1 | code2 | label (0/1) | functionality_id
 def fetch_functionality_data(fetch_clones=True, max_rows_per_functionality=0):
-    
+    import psycopg2
+
     # Database connection parameters
     db_params = {
         "host"    : "localhost",
@@ -55,7 +55,7 @@ def fetch_functionality_data(fetch_clones=True, max_rows_per_functionality=0):
     return data
 
 def get_functionality_data(max_rows_per_functionality):
-    data_pairs_all_fname       = cache_dir + 'bcb_1vsAll/data_pairs_all_' + str(max_rows_per_functionality) + '.pickle'
+    data_pairs_all_fname = cache_dir + 'bcb_1vsAll/data_pairs_all_' + str(max_rows_per_functionality) + '.pickle'
 
     if os.path.isfile(data_pairs_all_fname):
         data_pairs_all = pd.read_pickle(data_pairs_all_fname)
@@ -63,11 +63,39 @@ def get_functionality_data(max_rows_per_functionality):
     else:
         data_true      = fetch_functionality_data(fetch_clones=True , max_rows_per_functionality=max_rows_per_functionality)
         data_false     = fetch_functionality_data(fetch_clones=False, max_rows_per_functionality=max_rows_per_functionality)
+        
         # Avoid creating unbalanced dataset
-        n_rows_to_keep = min(len(data_true), len(data_false))
-        data_true      = data_true.sample(n_rows_to_keep)
-        data_false     = data_false.sample(n_rows_to_keep)
-        data_pairs_all = pd.concat([data_true, data_false]).reset_index()
+        # n_rows_to_keep = min(len(data_true), len(data_false))
+        # data_true      = data_true.sample(n_rows_to_keep)
+        # data_false     = data_false.sample(n_rows_to_keep)
+        # data_pairs_all = pd.concat([data_true, data_false]).reset_index()
+
+
+        # Find common functionality_ids
+        common_functionality_ids = set(data_true['functionality_id']).intersection(set(data_false['functionality_id']))
+
+        # For each functionality_id, keep N true pairs and N false pairs where N is the
+        # minimum number of occurences of functionality_id in "data_true" and "data_false". E.g., 
+        # if for functionality_id=1 we fetched 30 true clones and 20 false clones, we will keep
+        # min(20,30)=20 ture clones and 20 false clones so that the dataset is balanced
+        data_pairs_all = pd.concat([
+            data_true[data_true['functionality_id'] == func_id].head(
+                min(
+                    data_true['functionality_id'].value_counts().get(func_id, 0),
+                    data_false['functionality_id'].value_counts().get(func_id, 0)
+                )
+            )
+            for func_id in common_functionality_ids
+        ] + [
+            data_false[data_false['functionality_id'] == func_id].head(
+                min(
+                    data_true['functionality_id'].value_counts().get(func_id, 0),
+                    data_false['functionality_id'].value_counts().get(func_id, 0)
+                )
+            )
+            for func_id in common_functionality_ids
+        ], ignore_index=True)
+
         data_pairs_all.to_pickle(data_pairs_all_fname)
 
     return data_pairs_all
